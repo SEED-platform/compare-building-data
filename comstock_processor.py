64a92f539f151fc103f07f74730eee3f6a7ef104
Nicholas Long <nicholas.long@nrel.gov>
2024-06-20 14:47:14 -0600

    add poetry

--- comstock_processor.py
+++ comstock_processor.py
@@ -8,24 +8,24 @@ import pandas as pd
 import requests
 import os
 from pathlib import Path
-
+from pandas import DataFrame
 
 class ComStockProcessor:
-    def __init__(self, state, county_name, building_type, upgrade, base_dir):
+    def __init__(self, state: str, county_name: str, building_type: str, upgrade: str, base_dir: Path) -> None:
         self.state = state
         self.county_name = county_name
         self.building_type = building_type
         self.upgrade = upgrade
         self.base_dir = base_dir
 
-        if self.base_dir.exists() is False:
+        if not self.base_dir.exists():
             self.base_dir.mkdir()
             
         self.base_url = "https://oedi-data-lake.s3.amazonaws.com/nrel-pds-building-stock/end-use-load-profiles-for-us-building-stock/2023/comstock_amy2018_release_2/"
         self.metadata_url = self.base_url + "metadata/baseline.parquet"
         os.chdir(self.base_dir)
 
-    def download_file(self, url, save_path):
+    def download_file(self, url: str, save_path: Path) -> None:
         response = requests.get(url)
         if response.status_code == 200:
             with open(save_path, 'wb') as file:
@@ -34,9 +34,9 @@ class ComStockProcessor:
         else:
             print(f"Failed to download file: {url}")
 
-    def process_metadata(self):
-        self.download_file(self.metadata_url, "comstock_metadata.parquet")
-        meta_df = pd.read_parquet("comstock_metadata.parquet")
+    def process_metadata(self) -> DataFrame:
+        self.download_file(self.metadata_url, self.base_dir / "comstock_metadata.parquet")
+        meta_df = pd.read_parquet(self.base_dir / "comstock_metadata.parquet")
         meta_df.reset_index(drop=False, inplace=True)
         
         lookup_county = f"{self.state}, {self.county_name}"
@@ -45,23 +45,23 @@ class ComStockProcessor:
             (meta_df['in.comstock_building_type'] == self.building_type)
         ]
         
-        output_csv = f"{self.state}-{self.county_name}-{self.building_type}-{self.upgrade}-selected_metadata.csv"
+        output_csv = self.base_dir / f"{self.state}-{self.county_name}-{self.building_type}-{self.upgrade}-selected_metadata.csv"
         meta_df.to_csv(output_csv, index=False)
         return meta_df
 
-    def process_building_time_series(self, meta_df):
+    def process_building_time_series(self, meta_df: DataFrame) -> None:
         for index, row in meta_df.iterrows():
             building_id = str(row['bldg_id'])
             print(f"Now Processing {building_id}")
             building_time_series_file = f"{self.base_url}timeseries_individual_buildings/by_state/upgrade={self.upgrade}/state={self.state}/{building_id}-{self.upgrade}.parquet"
-            save_path = f"{building_id}-{self.upgrade}.parquet"
+            save_path = self.base_dir / f"{building_id}-{self.upgrade}.parquet"
             self.download_file(building_time_series_file, save_path)
             
             tdf = pd.read_parquet(save_path)
-            tdf.to_csv(f"{building_id}-{self.upgrade}.csv", index=False)
+            tdf.to_csv(self.base_dir / f"{building_id}-{self.upgrade}.csv", index=False)
         print("Finished")
 
-def main():
+def main() -> None:
     # Settings for modification
     state = 'OR'
     county_name = 'Multnomah County'
