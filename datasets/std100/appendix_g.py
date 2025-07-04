906f843362e817ac9e1d2483ab80a35346ffd364
Nicholas Long <nicholas.long@nrel.gov>
2025-07-04 09:17:51 -0600

    cleanup std100 directory

--- datasets/std100/appendix_g.py
+++
@@ -1,4 +0,0 @@
-# this needs to be updated. Create a class that can read in the std100data.xlsx file
-# and create a lookup such as
-#   std100.table_g_1['Office']['Climate Zone 7']
-# which returns the value.
